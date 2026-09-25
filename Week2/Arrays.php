a6e5788c78c0e91bdcb290e863c761e274e72b5c
Abdumudalib_12 <abdumudalib1227@gmail.com>
2026-09-25 13:00:53 -0700

    Addign Assigments 4 ---- 10 One time Push

---
+++ Week2/Arrays.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+
+$collection []= 2;
+$collection []=  " CA2313 ";
+
+// $collection [0]= 1;
+// $collection [1]= " Abdumudalib";
+// $collection [2]=  " CA2313 ";
+
+// foreach ( $collection as $list) 
+
+// {
+//     echo " List <br>";
+
+// }
+
+// echo "$collection[0] <br>";
+// echo "$collection [1] <br>";
+// var_dump ($collection);
+
+
+
+// // create array and initialization in one time
+// $number =array ( 2 , " Abdumudalib" , " CA2313");
+
+// //  adding item of array / Replace
+
+
+
+// example of associative array
+
+$info = array(
+    "ID" => 2,
+    "Name" => "Abdumudalib",
+    "Age" => 20,
+    "address" => "Mogadishu Banadir",
+    "Status" => "Single"
+// displaying the array 
+
+echo " <pre>";
+echo " Information of the student <br>";
+print_r ($info);
+var_dump ($info);
+echo " </pre>";
+
+)
+
+
+?>
+
+</body>
+</html>
