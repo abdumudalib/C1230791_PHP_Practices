bcf3216791749c6130e30e9d47f03ddef3ad6474
Abdumudalib_12 <abdumudalib1227@gmail.com>
2026-09-24 23:01:50 -0700

    Week2 Practices NastedLoop

---
+++ Week2/LoopPractice.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+
+
+// Example Of Do while 
+
+// $Result = 1;
+// $n = 5 ;
+
+// do {
+
+// $Result *= $n ;
+// echo " The value of in is $n <br>" ;
+// $n--;
+
+// }while ( $n > 0 );
+// echo $result;
+
+// Examples In Nate Loop 
+
+for ( $i = 1 ; $i<= 5 ; $i++)
+    for( $j =1 ; $j<= 5 ; $j++)
+        echo( " row $i , Column $j = " . "result is" . ($i * $j) . "<br>");
+?>
+
+</body>
+</html>
